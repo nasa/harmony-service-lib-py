b6b267a2d544dec03a5746e79bd5fa6932a63360
vinny <vinny.inverso@gmail.com>
2022-10-04 09:44:52 -0400

    HARMONY-1204: Add a default logger name

--- harmony/logging.py
+++ harmony/logging.py
@@ -61,7 +61,7 @@ class RedactorFormatter(object):
 
 
 @lru_cache(maxsize=128)
-def build_logger(config, name=None, stream=None):
+def build_logger(config, name='harmony-service', stream=None):
     """
     Builds a logger with appropriate defaults for Harmony
     Parameters
@@ -78,7 +78,7 @@ def build_logger(config, name=None, stream=None):
     logger : Logging
         A logger for service output
     """
-    logger = logging.getLogger('harmony')
+    logger = logging.getLogger(name)
     syslog = logging.StreamHandler(stream)
     if config.text_logger:
         formatter = logging.Formatter("%(asctime)s [%(levelname)s] [%(name)s.%(funcName)s:%(lineno)d] %(message)s")
